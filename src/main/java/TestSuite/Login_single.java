9165c05ab5d76cb6ee815ba86ed37d2e692ab174
Lily Hank <lilyhank1201@gmail.com>
2026-01-08 17:54:34 +0700

    update code asset done

--- src/main/java/TestSuite/Login_single.java
+++ src/main/java/TestSuite/Login_single.java
@@ -1,66 +1,59 @@
 package TestSuite;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.Test;
-import org.testng.AssertJUnit;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriver.Timeouts;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.AssertJUnit;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import org.testng.Assert;
 
 import Common.CommonBase;
 import Constant.CT_Account;
 import pageLocator.Asset_overview_location;
 import pageLocator.Login_locator;
-import pageLocator.OtpPage; 
+import pageLocator.OtpPage;
 
 public class Login_single extends CommonBase {
 	WebDriver driver;
 	WebDriverWait wait;
 	Login_locator loginPage;
 	OtpPage otpPage;
-	
+
 	@BeforeMethod
 	public void openChromeDriver() {
 		driver = initChromeDriver(CT_Account.webURL_login);
 		otpPage = new OtpPage(driver);
 		loginPage = new Login_locator(driver);
+		Timeouts pageLoadTimeout = driver.manage().timeouts().pageLoadTimeout(100, TimeUnit.SECONDS);
 	}
 
 	@Test(priority = 1)
-	public void Transfer_SS() throws InterruptedException {
+	public void TS_FFundingToTrading () throws InterruptedException {
 		// login
 		Login_locator loginPage = new Login_locator(driver);
 		loginPage.LoginFunction_Email_SS_test("stg2@mailinator.com", "Te@12345");
-		AssertJUnit.assertTrue(loginPage.checkSuccessMsgIsDisplayed(), "LỖI: Thông báo thành công không hiển thị!");
+		AssertJUnit.assertTrue(loginPage.checkSuccessMsgIsDisplayed());
 		otpPage.waitOtpVisible();
 		otpPage.enterOtp("123456");
 		boolean isLoginSuccess = loginPage.verifyloginSS();
-		AssertJUnit.assertTrue("LỖI: Không vào được trang chủ!" + "URL hiện tại: " + driver.getCurrentUrl(), isLoginSuccess); 
-	// goto Assets > overview
-		driver.get(CT_Account.Assets_transfer);  
-	// Go to transfer	
-		Asset_overview_location openAssets = new Asset_overview_location(driver);  
+		AssertJUnit.assertTrue("LỖI: Không vào được trang chủ!" + "URL hiện tại: " + driver.getCurrentUrl(),
+				isLoginSuccess);
+		// goto Assets > overview
+		driver.get(CT_Account.Assets_transfer);
+		// Go to transfer
+		Asset_overview_location openAssets = new Asset_overview_location(driver);
 		openAssets.getAvailableBalance();
-	
-		openAssets.handleTransferAmount("0.01");  
-		openAssets.ClickTransferAsset( );
-	} 
- 
+		openAssets.handleTransferAmount("41");
+		openAssets.ClickTransferAsset();
+	}
+
 	@AfterMethod
 	public void tearDown() {
 		if (driver != null) {
 			driver.quit();
 		}
-	}}
-
-//} 
-//tôi  đang  xây dựng tại pageLocator cho asset  trước  
-//Check  balacne "available_balance_text":
-//- nếu "available_balance_text"* > 0  thì nhập vào amount_transfer_text
-//- nếu "available_balance_text" = 0 thì
-//+  in ra số dư = 0
-//+ và thực hiện  click  vào  "icon_swap" >>>  rồi  check balacne  "available_balance_text" ( nếu  "available_balance_text" = 0 *thì* in *ra* số dư = 0, nếu "available_balance_text" > 0 thì nhập vào amount_transfer_text )
+	}
+}
--- src/main/java/pageLocator/Asset_overview_location.java
+++ src/main/java/pageLocator/Asset_overview_location.java
@@ -27,12 +27,6 @@ public class Asset_overview_location {
 	private WebElement GotoTransfer;
 	@FindBy(xpath = "(//*[@type='text'])[3]")
 	private WebElement amount_transfer_text;
-	// span[contains(@class, 'button-2')]
-	// span[contains(@class, 'button-2') and contains(.., 'Khả dụng')]
-	// span[@class='button-2 text-neutral-700']
-	// (//span[@class='button-2 text-neutral-700'])[1]
-	@FindBy(xpath = "//span[contains(@class, 'button-2')]")
-	public WebElement available_balance_text;
 
 	// (//*[name()='svg' and @width='25' and @height='25'])[last()]
 	// div[2]/div[2]/*[name()='svg'][1]
@@ -43,6 +37,17 @@ public class Asset_overview_location {
 	@FindBy(xpath = "(//span[contains(text(),'Chuyển tài sản')])[1]")
 	private WebElement TransferBtn;
 	private WebElement transferBtn01;
+
+	@FindBy(xpath = "(//div[@class='button-1'])[1]")
+	private WebElement NameAssetDefault;
+	@FindBy(xpath = "span[title='Ví Funding']")
+	private WebElement FromAssetDefault;
+	// span[contains(@class, 'button-2')]
+	// span[contains(@class, 'button-2') and contains(.., 'Khả dụng')]
+	// span[@class='button-2 text-neutral-700']
+	// (//span[@class='button-2 text-neutral-700'])[1]
+	@FindBy(xpath = "//span[contains(@class, 'button-2')]")
+	public WebElement available_balance_text;
 
 	public Asset_overview_location(WebDriver driver) {
 		this.driver = driver;
@@ -60,35 +65,96 @@ public class Asset_overview_location {
 	}
 
 	public double getAvailableBalance() {
-		// String balanceText = available_balance_text.getText().replaceAll("[^0-9.]",
-		// "").trim();
-		// return balanceText.isEmpty() ? 0.0 : Double.parseDouble(balanceText);
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(50));
-//		String availableBalanceCurrnet = available_balance_text.getText();
-		WebElement AvailableTxt = wait
-				.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[contains(@class, 'button-2')]")));
-		String availableBalanceCurrent = AvailableTxt.getText().replaceAll("[^0-9.]", "").trim();
-//		WebElement spanButton = wait
-//				.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[contains(@class, 'button-2')]")));
-		return 0;
+		wait.until(ExpectedConditions.visibilityOf(available_balance_text));
+		return Double.parseDouble(available_balance_text.getText().trim().replaceAll("[^0-9.]", ""));
 	}
-
-//	public void AvailableOld ()
-//	{
-//		PageFactory.initElements(new AjaxElementLocatorFactory(driver, 50), this);
-//		web
+//	public void printAllBalanceInfo() {
+//	    System.out.println("=== THÔNG TIN TÀI KHOẢN ===");
+// 	    System.out.println("Asset Default: " + NameAssetDefault.getText().trim().replaceAll("[^0-9.]", ""));
+//     System.out.println("Ví Funding: " + FromAssetDefault.getText().trim());
+//	    System.out.println("Available Balance: " + available_balance_text.getText().trim().replaceAll("[^0-9.]", ""));
+//	    System.out.println("===========================");
 //	}
 
-	public void ClickTransferAsset() {
+	public void handleTransferAmount(String amount) throws InterruptedException {
 
-		PageFactory.initElements(new AjaxElementLocatorFactory(driver, 50), this);
-//		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-//		wait.until(ExpectedConditions.elementToBeClickable(TransferBtn));
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
+		double balanceBefore = getAvailableBalance();
 
-		WebElement transferBtn = wait.until(ExpectedConditions
-				.elementToBeClickable(By.xpath("//button[.//span[contains(text(),'Chuyển tài sản')]]")));
-		transferBtn.click();
-		System.out.println("Transfer Success");
+		System.out.println("số dư hiện tại " + balanceBefore);
+		if (balanceBefore > 0) {
+			try {
+				WebElement input = wait.until(ExpectedConditions.visibilityOf(amount_transfer_text));
+
+				// Scroll vào view
+				((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView({block: 'center'});", input);
+
+				// Click để focus
+				input.click(); 
+				input.sendKeys(Keys.CONTROL + "a"); // Select all
+				input.sendKeys(Keys.DELETE); // Xóa hết
+
+				// Nhập amount mới
+				input.sendKeys(amount);
+
+				System.out.println("Đã nhập thành công số lượng: " + amount);
+
+			} catch (Exception e) {
+				throw new RuntimeException("Không thể nhập amount transfer", e);
+			}
+		} else {
+			System.out.println("Số dư = 0");
+//
+//			// Click swap icon
+//			wait.until(ExpectedConditions.elementToBeClickable(icon_swap)).click();
+//
+//			// Đợi balance cập nhật sau swap
+//			wait.until(ExpectedConditions.visibilityOf(available_balance_text));
+//			balance = printAllBalanceInfo();
+//
+//			if (balance > 0) {
+//				System.out.println("Sau khi swap, số dư: " + balance + " > 0 → Nhập amount");
+//				enterAmountIfPossible(amountToTransfer);
+//			} else {
+//				System.out.println("Sau khi swap, số dư vẫn = 0");
+//				// Có thể throw exception hoặc return false nếu cần fail test
+//			}
+		}
+	}
+
+	public void ClickTransferAsset() throws InterruptedException {
+		PageFactory.initElements(new AjaxElementLocatorFactory(driver, 60), this);
+		double balance = getAvailableBalance();
+		if (balance > 0) {
+			WebElement transferBtn01 = wait.until(ExpectedConditions
+					.elementToBeClickable(By.xpath("//span[contains(text(),'Chuyển tài sản')]//ancestor::button")));
+			((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView({block: 'center'});",
+					transferBtn01);
+			Thread.sleep(2000);
+			boolean transferbtnEnable = transferBtn01.isEnabled();
+			if (transferbtnEnable) {
+				System.out.println("Transfer button Enabled");
+
+			} else {
+				System.out.println("Transfer button NOT enable");
+			}
+			((JavascriptExecutor) driver).executeScript("arguments[0].click();", transferBtn01);
+
+			System.out.println("Click Transfer Success");
+			driver.navigate().refresh();
+			double balanceAfterInput = getAvailableBalance();
+			if (balanceAfterInput < balance) {
+				System.out.println("Transfer SUCCESS is REAL với balance new = " + balanceAfterInput);
+
+			} else {
+				System.out.println(
+						"Transfer FAILED hoặc chưa cập nhật: trước = " + balance + ", sau = " + balanceAfterInput); 
+			}
+		} else { 
+			System.out.println(" Not enter Amount transfer");
+		}
+
 	}
 
 	public void clickSwapIcon() {
@@ -96,65 +162,5 @@ public class Asset_overview_location {
 		wait.until(ExpectedConditions.elementToBeClickable(icon_swap));
 		icon_swap.click();
 	}
-	// Method: Nhập amount nếu balance > 0
-//	public void enterAmountIfPossible(String amount) {
-//		amount_transfer_text.clear();
-//		amount_transfer_text.sendKeys(amount);
-//	}
-	// enterAmountIfPossible
 
-	public void enterAmountIfPossible(String amount) throws InterruptedException {
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-
-		try {
-			WebElement input = wait.until(ExpectedConditions.visibilityOf(amount_transfer_text));
-
-			// Scroll vào view
-			((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView({block: 'center'});", input);
-
-			// Click để focus
-			input.click();
-
-			// Clear sạch (Ant Design hay không clear bình thường)
-			input.sendKeys(Keys.CONTROL + "a"); // Select all
-			input.sendKeys(Keys.DELETE); // Xóa hết
-
-			// Nhập amount mới
-			input.sendKeys(amount);
-
-			System.out.println("Đã nhập thành công số lượng: " + amount);
-
-		} catch (Exception e) {
-			throw new RuntimeException("Không thể nhập amount transfer", e);
-		}
-	}
-
-	// Method chính xử lý toàn bộ logic bạn mô tả
-	public void handleTransferAmount(String amountToTransfer) throws InterruptedException {
-
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
-		double balance = getAvailableBalance();
-
-		if (balance > 0) {
-			System.out.println("Số dư available: " + balance + " > 0 → Nhập amount");
-			enterAmountIfPossible(amountToTransfer);
-		} else {
-			System.out.println("Số dư = 0");
-
-			// Click swap icon
-			wait.until(ExpectedConditions.elementToBeClickable(icon_swap)).click();
-
-			// Đợi balance cập nhật sau swap
-			wait.until(ExpectedConditions.visibilityOf(available_balance_text));
-			balance = getAvailableBalance();
-
-			if (balance > 0) {
-				System.out.println("Sau khi swap, số dư: " + balance + " > 0 → Nhập amount");
-				enterAmountIfPossible(amountToTransfer);
-			} else {
-				System.out.println("Sau khi swap, số dư vẫn = 0");
-				// Có thể throw exception hoặc return false nếu cần fail test
-			}
-		}
-	}
 }
